ae05325ca452ba686b972a3f5bc9c02cdf567d3c
Yuki Iwanaga <yuki@creasty.com>
2013-11-25 11:19:47 +0900

    rails new した

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marcierge::Application.config.secret_token = 'bdb8abbc3acda4d824d915bfe031f3d1d19c627bbcdd9d8293a6c40af08aafc7290b29f7e662bb0ebc53ffd523ed47b52d09582143c546f2db602af581874580'
