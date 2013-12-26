b9e4d35622c1741907533fcfb1eaf2f43018fdc2
Yuki Iwanaga <yuki@creasty.com>
2013-12-26 15:44:40 +0900

    モデルはこんな感じで行こうと思っています

--- generate_models.sh
+++ generate_models.sh
@@ -1,21 +1,95 @@
 #!/bin/bash
 
+#  ログインユーザ
+#-----------------------------------------------
 
-rails g model event \
+# devise の認証情報を一元管理
+
+# has_one :admin
+# has_one :creator
+rails g devise user
+
+
+#  管理ユーザ
+#-----------------------------------------------
+
+# belongs_to :user
+rails g admin \
   name:string \
-  creator_id:integer \
+  user_id:integer
 
+
+#  クリエータ
+#-----------------------------------------------
+
+# リリース後、creator 自身がアカウントを作ったら
+# アソシエーションで紐付けられるようにする
+
+# belongs_to :user
+# has_one :avator, class_name: '::Medium'
+# has_many :events
 rails g model creator \
+  user_id:integer \
   name:string \
-  profile:text \
-  avatar_file_name:string \
-  avatar_content_type:string \
-  avatar_file_size:integer \
-  avatar_updated_at:datetime
+  profile:text \ # プロフィールページで表示される紹介文
+  avator_id:integer
+
+
+#  イベント
+#-----------------------------------------------
+
+# belongs_to :creator
+# has_many :event_dates, dependent: :destroy
+rails g model event \
+  creator_id:integer \
+  title:string \
+  content:text \
+  status:integer
+
+
+#  イベントの開催日時
+#-----------------------------------------------
+
+# 連続しない複数の期間や日によって時間が異なる場合を考慮し
+# has_many - belongs_to アソシエーションにしている
+
+# belongs_to :event
+rails g model event_date \
+  event_id:integer \
+  begin_at:datetime \
+  end_at:datetime
+
+
+#  メディア
+#-----------------------------------------------
+
+# 画像等のメディアはこのモデルで一元管理
+rails g model medium
+
+# paperclip を使う
+rails g paperclip medium asset
+
+
+#  ニュース
+#-----------------------------------------------
 
 rails g model information \
   title:string \
-  content:text
+  content:text \
+  status:integer
 
 
+#  特集
+#-----------------------------------------------
+
+rails g model feature \
+  title:string \
+  content:text \
+  status:integer
+
+
+#  タグ
+#-----------------------------------------------
+
+rails g acts_as_taggable_on:migration
 
