d0ea0676a73484cda5bd1d68941ea6124c5d3004
hatch2 <hiroaki@hatch2.com>
2014-04-21 01:57:34 +0900

    unicornの再起動

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,5 +63,5 @@ after 'deploy', 'deploy:migrate'
 after 'deploy', 'deploy:cleanup'
 after 'deploy:create_symlink', 'deploy:symlink_contents'
 # after 'deploy:restart', 'unicorn:reload'    # app IS NOT preloaded
-# after 'deploy:restart', 'unicorn:restart'   # app preloaded
-after 'deploy:restart', 'unicorn:duplicate' # before_fork hook implemented (zero downtime deployments)
+after 'deploy:restart', 'unicorn:restart'   # app preloaded
+# after 'deploy:restart', 'unicorn:duplicate' # before_fork hook implemented (zero downtime deployments)
