fd8e7e668a991103c5b66b73e3b2890b22e84964
hatch2 <hiroaki@hatch2.com>
2014-04-29 14:01:37 +0900

    背景

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,8 @@ namespace :deploy do
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{current_release}/tmp/restart.txt"
+    run "cd #{current_release}; RAILS_ENV=production bundle exec rake unicorn:restart"
+    # run "touch #{current_release}/tmp/restart.txt"
   end
 end
 
@@ -57,7 +58,7 @@ end
 
 after 'deploy', 'deploy:migrate'
 after 'deploy:create_symlink', 'deploy:symlink_contents'
-after 'deploy:restart', 'unicorn:reload'    # app IS NOT preloaded
+# after 'deploy:restart', 'unicorn:reload'    # app IS NOT preloaded
 # after 'deploy:restart', 'unicorn:restart'   # app preloaded
 after 'deploy:restart', 'unicorn:duplicate' # before_fork hook implemented (zero downtime deployments)
 after 'deploy', 'deploy:cleanup'
